3fe1adaa936182f7309b9f14cb3c8e92cb9251af
Sammy <sammy@MCRN-01.net>
2019-01-02 16:58:16 -0600

    Misc Updates

--- baseball_functions.py
+++ baseball_functions.py
@@ -73,7 +73,7 @@ def batting_order():
 
 
 def pitch_result():
-    # pitch_result_tuple = (('strike', 10), ('ball', 11), ('foul ball', 12), ('foul out', 13), ('out - defense', 14),
-    #                       ('hit - single', 1), ('hit - double', 2), ('hit - triple', 3), ('hit - homerun', 4))
+    pitch_result_tuple = (('strike', 10), ('ball', 11), ('foul ball', 12), ('foul out', 13), ('out - defense', 14),
+                          ('hit - single', 1), ('hit - double', 2), ('hit - triple', 3), ('hit - homerun', 4))
     pitch_result_return = random.randint(0, 8)
     return pitch_result_tuple[pitch_result_return]
---
+++ test_file.py
@@ -0,0 +1,18 @@
+import baseball_functions
+import random
+
+
+# def pitch_result():
+#     pitch_result_tuple = (('strike', 10), ('ball', 11), ('foul ball', 12), ('foul out', 13), ('out - defense', 14),
+#                           ('hit - single', 1), ('hit - double', 2), ('hit - triple', 3), ('hit - homerun', 4))
+#     pitch_result_return = random.randint(0, 8)
+#     return pitch_result_tuple[pitch_result_return]
+
+
+pitch_check = pitch_result()
+type, idx = pitch_check
+print(pitch_check)
+print(type, idx)
+
+xxxx = batting_order()
+
