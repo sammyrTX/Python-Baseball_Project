6ec196af4c05c20b1ce5df03828dde12a0e667a0
Sam Rivera <samrivera.contact@gmail.com>
2020-02-05 13:02:25 -0600

    Fix spacing fpr scorebox print out

--- scorebox_functions/scorebox.py
+++ scorebox_functions/scorebox.py
@@ -15,35 +15,35 @@ def print_scorebox(current_inning_f, visitors_list, home_list):
     print('INNING  ', end='')
 
     for inning_counter in range(0, current_inning_f):
-        print(f'\t{(inning_counter + 1):2}', end='')
+        print(f'  {(inning_counter + 1):2}', end='')
 
-    print('\t R', end='')
-
-    print()
-
-    print('------     ', end='')
-
-    for inning_counter in range(0, current_inning_f):
-        print('\t--', end='')
-
-    print('\t--', end='')
+    print('   R', end='')
 
     print()
 
-    print('VISITORS ', end='')
+    print('------  ', end='')
 
     for inning_counter in range(0, current_inning_f):
-        print(f'\t{(visitors_list[inning_counter]):2}', end='')
+        print('  --', end='')
 
-    print(f'\t{(sum(visitors_list)):2}', end='')
+    print('  --', end='')
+
+    print()
+
+    print('VISITORS', end='')
+
+    for inning_counter in range(0, current_inning_f):
+        print(f'  {(visitors_list[inning_counter]):2}', end='')
+
+    print(f'  {(sum(visitors_list)):2}', end='')
 
     print()
 
     print('HOME    ', end='')
 
     for inning_counter in range(0, current_inning_f):
-        print(f'\t{(home_list[inning_counter]):2}', end='')
+        print(f'  {(home_list[inning_counter]):2}', end='')
 
-    print(f'\t{(sum(home_list)):2}', end='')
+    print(f'  {(sum(home_list)):2}', end='')
 
     print('\n\n')
