6552d95eead78ad2a61b86faf703932252a76f96
Sam Rivera <samrivera.contact@gmail.com>
2020-02-11 18:21:17 -0600

    Set up test file
    Test file operates after adding __init__.py to folders along path to
    test folder up to main Python-Baseball_Project folder.

--- test/test00.py
+++ test/test00.py
@@ -2,7 +2,10 @@
 Test Suite
 """
 from baseball_funcs.game_set_up import (team_description_func,
+                                        players_tuple_func,
                                         )
+
+print("=" * 70)
 
 print(f'TESTING!!!')
 
@@ -11,5 +14,24 @@ def test00_00():
 
     assert team_description_func == ('VISITORS', 'HOME')
 
+
+def test00_01():
+
+    xy = 232
+
+    assert xy == 232
+
+
+def test00_02():
+
+    assert players_tuple_func == ("Pitcher",
+                                  "Catcher",
+                                  "First Base",
+                                  "Second Base",
+                                  "Third Base",
+                                  "Shortstop",
+                                  "Left Field",
+                                  "Center Field",
+                                  "Right Field",)
 
 print('End Testing...!')
